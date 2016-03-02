9d1663bea8be9666a95736d49b4f826679c5ceda
alu0100819182 <alu0100819182@ull.edu.es>
2016-03-02 18:20:02 +0000

    Medida y temperatura

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,52 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
+    var regexp = /[-+]?\d+(?:\.\d+)?)\s*([a-zA-Z]+)/;
+    if(valor.match(regexp))
+    {
+      valor = valor.match(regexp);
+      this.valor = valor[1];
+      this.tipo = valor[2];
+    }
+    else
+    {
+      this.valor = valor;
+      this.tipo = tipo;
+    }
   }
-  
+
+  Temperatura.prototype = new Medida ();
+  Temperatura.prototype.constructor = Temperatura;
+
   function Temperatura(valor,tipo)
   {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    var regexp = /^\s([-+]?\d+(?:.\d)?)(?:([eE])[+-]?(\d)+)?\s(f|c|k)\s$/i
+    if(valor.match(regexp))
+    {
+      valor = valor.match(regexp);
+      this.valor = valor[1];
+      this.tipo = valor[2];
+    }
+    else
+    {
+      this.valor = valor;
+      this.tipo = tipo;
+    }
   }
-  
+
   function Celsius(valor)
   {
   }
-  
+
   function Farenheit(valor)
   {
   }
-  
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -30,14 +57,14 @@
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
     valor     = valor.match(regexp);
-    
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,7 +74,7 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
